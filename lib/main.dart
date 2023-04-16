144e7edf2113fdab9e0e5edbcce84cb0ab2d1fdb
Dario Alarcon <darioalarcon832@gmail.com>
2023-04-16 17:10:05 -0500

    se agrego la base de datos exitosamente

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 import 'platzi_trips.dart';
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
